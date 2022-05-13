cea6bb3ca809631b09c2f174051a684564ff26ea
Hopatsa-Oleksandr <bc.alexg@gmail.com>
2022-05-13 22:08:20 +0300

    +++

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-$(window).keyup(function(e){
-	var target = $('.checkbox-other input:focus');
-	if (e.keyCode == 9 && $(target).length){
-		$(target).parent().addClass('focused');
-	}
-});
+// $(window).keyup(function(e){
+// 	var target = $('.checkbox-other input:focus');
+// 	if (e.keyCode == 9 && $(target).length){
+// 		$(target).parent().addClass('focused');
+// 	}
+// });
 
-$('.checkbox-other input').focusout(function(){
-	$(this).parent().removeClass('focused');
-});
+// $('.checkbox-other input').focusout(function(){
+// 	$(this).parent().removeClass('focused');
+// });
